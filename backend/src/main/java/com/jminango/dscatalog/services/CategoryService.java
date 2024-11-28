39d44784d6cdbec9efd11a3e3c53d2d20e6364e1
Juan Minango <juan.minango@bbchain.com.br>
2024-11-28 11:40:27 -0300

    feat: delete category delete

--- backend/src/main/java/com/jminango/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/jminango/dscatalog/services/CategoryService.java
@@ -3,9 +3,12 @@ package com.jminango.dscatalog.services;
 import com.jminango.dscatalog.dto.CategoryDto;
 import com.jminango.dscatalog.entities.Category;
 import com.jminango.dscatalog.repositories.CategoryRepository;
+import com.jminango.dscatalog.services.exceptions.DataBaseException;
 import com.jminango.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -48,6 +51,16 @@ public class CategoryService {
             return new CategoryDto(entity);
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataBaseException("Integrity violation");
         }
     }
 }
