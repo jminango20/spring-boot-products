752cba48431d6932c213887273f02ec1e651c694
Juan Minango <juan.minango@bbchain.com.br>
2024-12-21 17:41:48 -0300

    test: Product Service Integration Tests - FindAllPage

--- backend/src/test/java/com/jminango/dscatalog/services/ProductServiceIntegrationTest.java
+++ backend/src/test/java/com/jminango/dscatalog/services/ProductServiceIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.jminango.dscatalog.services;
 
+import com.jminango.dscatalog.dto.ProductDto;
 import com.jminango.dscatalog.repositories.ProductRepository;
 import com.jminango.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
@@ -7,6 +8,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 @SpringBootTest
 public class ProductServiceIntegrationTest {
@@ -40,6 +44,39 @@ public class ProductServiceIntegrationTest {
         Assertions.assertThrows(ResourceNotFoundException.class, () -> {
             service.delete(nonExistentId);
         });
+    }
+
+    @Test
+    public void findAllPaginatedShouldReturnPageWhenPageNumberIsZeroAndPageSizeIsTen() {
+        PageRequest pageable = PageRequest.of(0, 10);
+        Page<ProductDto> result = service.findAllPaginated(pageable);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(0, result.getNumber());
+        Assertions.assertEquals(10, result.getSize());
+        Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+    }
+
+    @Test
+    public void findAllPaginatedShouldReturnEmptyPageWhenPageDoesNotExist() {
+        PageRequest pageable = PageRequest.of(50, 10);
+        Page<ProductDto> result = service.findAllPaginated(pageable);
+
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void findAllPaginatedSortByNameShouldReturnSortedPage() {
+
+        PageRequest pageable = PageRequest.of(0, 10, Sort.by("name"));
+        Page<ProductDto> result = service.findAllPaginated(pageable);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+        Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+        Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
     }
 
 }
